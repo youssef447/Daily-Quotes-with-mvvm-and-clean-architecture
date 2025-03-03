0b18783273aa1b4010b5613dde1b957c69c31a79
youssef447 <youssef.ashraf380@gmail.com>
2025-03-03 05:57:31 +0200

    feat: rate limit interceptor

---
+++ lib/core/services/Network/remote/rate_limit_interceptor.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+
+class RateLimitingInterceptor extends Interceptor {
+  final int maxRequests = 5;
+  final Duration timeFrame = Duration(seconds: 15);
+  final List<DateTime> requestTimes = [];
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    final now = DateTime.now();
+    requestTimes.add(now);
+
+    // remove old requests from list.
+    requestTimes.removeWhere(
+      (time) => now.difference(time) > timeFrame,
+    );
+
+    if (requestTimes.length > maxRequests) {
+      return handler.reject(
+        DioException(
+          requestOptions: options,
+          error: 'Too many requests. Please try again later.',
+        ),
+      );
+    }
+  }
+}
